05631fdd966a06ada40e9fc26f9134153de88f44
nitesh8652 <niteshsalian8652085233@gmail.com>
2026-03-16 20:49:11 +0530

    checking ai api through postman

--- Backend/src/Controllers/Interview.controller.js
+++ Backend/src/Controllers/Interview.controller.js
@@ -2,29 +2,33 @@ const pdfParse = require("pdf-parse")
 const generateInterviewReport = require("../Services/Ai.Service.js")
 const interviewReportModel = require("../Models/InterviewReport.js")
 
-async function generateInterViewReportController(req,res){
+async function generateInterViewReportController(req, res) {
 
-    const resumeContent= await(new pdfParse.PDFParse(Uint8Array.from(req.file.buffer))).getText()
-    const {selfDescription, jobDescription} = req.body
+    const resumeContent = await (new pdfParse.PDFParse(Uint8Array.from(req.file.buffer))).getText()
+    const { selfDescription, jobDescription } = req.body
 
     const interviewReportByAi = await generateInterviewReport({
-        resume:resumeContent.text,
+        resume: resumeContent.text,
         selfDescription,
         jobDescription
     })
 
     const interviewReport = await interviewReportModel.create({
-        user:req.user._id,
-        resume:resumeContent.text,
+        user: req.user._id,
+        resume: resumeContent.text,
         selfDescription,
         jobDescription,
-        ...interviewReportByAi
+        matchScore: interviewReportByAi.matchScore,
+        technicalQuestion: interviewReportByAi.technicalQuestions,       // plural → singular
+        behaviouralQuestion: interviewReportByAi.behavioralQuestions,    // behavioral → behavioural
+        skillGaps: interviewReportByAi.skillGap,                         // singular → plural
+        preparationPlan: interviewReportByAi.preparationPlan,
 
     })
- 
+
     res.status(201).json({
-        success:true,
-        message:"Interview Report Generated Successfully",
+        success: true,
+        message: "Interview Report Generated Successfully",
         interviewReport
     })
 
@@ -33,4 +37,4 @@ async function generateInterViewReportController(req,res){
 
 
 
-module.exports = {generateInterViewReportController}
+module.exports = { generateInterViewReportController }
